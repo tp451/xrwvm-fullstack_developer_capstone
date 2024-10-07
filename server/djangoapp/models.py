3d1a10f078c72e5fe2109ad34ce11d821fa22888
Thorben <141824956+tp451@users.noreply.github.com>
2024-10-07 18:14:12 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,6 +8,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class CarMake(models.Model):
+
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
@@ -16,7 +18,8 @@ class CarMake(models.Model):
         return self.name  # Return the name as the string representation
 
 class CarModel(models.Model):
-    
+
+
     car_make = models.ForeignKey(
         CarMake,
         on_delete=models.CASCADE  # Many-to-One relationship
