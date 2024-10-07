af5eca88a0a593d340ea98c0302db9a05334b225
Thorben <141824956+tp451@users.noreply.github.com>
2024-10-07 17:58:53 +0200

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -21,10 +21,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -34,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -93,19 +93,19 @@ app.get("/fetchDealer/:id", async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  let new_id = documents[0].id + 1;
 
-  const review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
+const review = new Reviews({
+    id: new_id,
+    name: data.name,  // Changed to dot notation
+    dealership: data.dealership,  // Changed to dot notation
+    review: data.review,  // Changed to dot notation
+    purchase: data.purchase,  // Changed to dot notation
+    purchase_date: data.purchase_date,  // Changed to dot notation
+    car_make: data.car_make,  // Changed to dot notation
+    car_model: data.car_model,  // Changed to dot notation
+    car_year: data.car_year,  // Changed to dot notation
+});
 
   try {
     const savedReview = await review.save();
